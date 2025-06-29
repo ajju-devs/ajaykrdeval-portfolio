eb052e8b3406324347334cc02cd682078817ef82
Ajay Kumar <theajaykumar29@gmail.com>
2025-06-29 10:36:52 +0530

    First Commit
    Initial commit – complete portfolio

---
+++ src/constants.js
@@ -0,0 +1,233 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+
+import numpyLogo from './assets/tech_logo/numpy.png';
+import pandasLogo from './assets/tech_logo/pandas.png';
+import matplotlibLogo from './assets/tech_logo/matplotlib.png';
+import seabornLogo from './assets/tech_logo/seaborn.png';
+import plotlyLogo from './assets/tech_logo/plotly.jpg';
+import scikitlearnLogo from './assets/tech_logo/scikitlearn.png';
+import tensorflowLogo from './assets/tech_logo/tensorflow.png';
+import kerasLogo from './assets/tech_logo/keras.png';
+import pytorchLogo from './assets/tech_logo/pytorch.png';
+import nltkLogo from './assets/tech_logo/nltk.png';
+import spacyLogo from './assets/tech_logo/spacy.png';
+import opencvLogo from './assets/tech_logo/opencv.png';
+import pillowLogo from './assets/tech_logo/pillow.png';
+import yoloLogo from './assets/tech_logo/yolo.png';
+import streamlitLogo from './assets/tech_logo/streamlit.png';
+import apachesparkLogo from './assets/tech_logo/apachespark.png';
+import hadoopLogo from './assets/tech_logo/hadoop.png';
+import apachekafkaLogo from './assets/tech_logo/apachekafka.png';
+import langchainLogo from './assets/tech_logo/langchain.png';
+import faissLogo from './assets/tech_logo/faiss.png';
+import pineconeLogo from './assets/tech_logo/pinecone.png';
+import RAGLogo from './assets/tech_logo/RAG.jpg';
+import msofficeLogo from './assets/tech_logo/msoffice.png';
+import msexcelLogo from './assets/tech_logo/msexcel.png';
+import jupyterLogo from './assets/tech_logo/jupyter.png';
+
+// Experience Section Logo's
+import prodigalailogo from './assets/company_logo/prodigalai_logo.png';
+import ibmlogo from './assets/company_logo/ibm_logo.png';
+
+// Education Section Logo's
+import ggsipulogo from './assets/education_logo/ggsipu_logo.png';
+
+// Project Section Logo's
+import insightechoLogo from './assets/work_logo/insightecho.png';
+import thrivewellLogo from './assets/work_logo/thrivewell.png';
+import redditytcrudLogo from './assets/work_logo/redditytcrud.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Web Development',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Redux', logo: reduxLogo },      
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'AI-ML',
+    skills: [
+      { name: 'Python', logo: pythonLogo },
+      { name: 'Numpy', logo: numpyLogo },
+      { name: 'Pandas', logo: pandasLogo },
+      { name: 'Matplotlib', logo: matplotlibLogo },
+      { name: 'Seaborn', logo: seabornLogo },
+      { name: 'Plotly', logo: plotlyLogo },
+      { name: 'Scikit-learn', logo: scikitlearnLogo },
+
+      { name: 'TensorFlow', logo: tensorflowLogo },
+      { name: 'Keras', logo: kerasLogo },
+      { name: 'PyTorch', logo: pytorchLogo },
+      { name: 'NLTK', logo: nltkLogo },
+      { name: 'spaCy', logo: spacyLogo },
+      { name: 'OpenCV', logo: opencvLogo },
+      { name: 'PIL/Pillow', logo: pillowLogo },
+
+      { name: 'YOLO', logo: yoloLogo },
+      { name: 'Streamlit', logo: streamlitLogo },
+      { name: 'Apache Spark', logo: apachesparkLogo },
+      { name: 'Hadoop', logo: hadoopLogo },
+      { name: 'Apache Kafka', logo: apachekafkaLogo },
+      { name: 'LangChain', logo: langchainLogo },
+      { name: 'FAISS', logo: faissLogo },
+      { name: 'Pinecone', logo: pineconeLogo },
+      { name: 'RAG', logo: RAGLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+      { name: 'Jupyter', logo: jupyterLogo },
+      { name: 'MS Office', logo: msofficeLogo },
+      { name: 'MS Excel', logo: msexcelLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: prodigalailogo,
+      role: "Project Intern",
+      company: "Prodigal AI",
+      date: "August 2024 - December 2024",
+      desc: "Internship as part of the Automated Socials project. Developed and contibuted to dynamic and scalable CRUD based web application with automation features, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "Python",
+        "HTML",
+        "CSS",
+        "Tailwind CSS",
+        "Streamlit",
+        "Rest APIs",
+        "GoogleAPIclient",
+        "MySQL",
+        "OOP",
+      ],
+    },
+    {
+      id: 1,
+      img: ibmlogo,
+      role: "Frontend Development Intern",
+      company: "IBM",
+      date: "July 2024 - August 2024",
+      desc: "Contributed to innovative frontend based project as a frontend intern. Built and styled interactive components. Visualized user data through React dashboards to support better decision-making. Ensured accessibility and responsiveness across devices. Improved user experience by streamlining workflows. Used development technologies such as HTML, CSS, JavaScript, ReactJS, and Git. Worked closely with the team to deliver responsive, high-performance web application and improve user experience through seamless integration of various technologies.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "JavaScript",
+        "Tailwind CSS",
+        "HTML",
+        "CSS",
+        "Figma",
+        "Material UI",
+      ],
+    },
+    
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: ggsipulogo,
+      school: "Guru Gobind Singh Indraprastha University, Delhi",
+      date: "Graduated",
+      grade: "8.0 CGPA",
+      desc: "B.Tech in Artificial Intelligence & Machine Learning from GGSIPU, Delhi. Relevant courses: Programming, Software Development, Data Analysis, Artificial Intelligence, Machine Learning, Predictive Analytics, Cloud Computing, Natural Language Processing, Computer Vision, Data Structures & Algorithms, Object-Oriented Programming, Operating System, Computer Networking, Database Management System and Computer Science Principles.",
+      degree: "Bachelor of Technology - B.Tech",
+    },
+    
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "InsightEcho- News AI Assistant",
+      description:
+        "InsightEcho is a Retrieval-Augmented Generation (RAG) based AI-powered news assistant designed to help users make sense of complex articles with ease. This web application allows users to input URLs of news articles, which are then processed using advanced natural language understanding techniques. The backend extracts meaningful content, converts it into dense vector embeddings using SentenceTransformer, and stores it in FAISS for fast similarity search. Users can then engage in a chatbot-style conversation to ask questions about the article, powered by a Mistral-7B language model. The project features a clean and responsive frontend built with React.js and TailwindCSS, and a modular, high-performance backend using FastAPI — offering an intuitive, seamless, and intelligent reading experience.",
+      image: insightechoLogo,
+      tags: ["Python", "JavaScript", "HTML", "CSS", "React.js", "TailwindCSS", " Vite", "FastAPI", "LangChain", "SentenceTransformer", "FAISS", "HuggingFace Endpoints", "Mistral-7B", "Git", "VSCode"],
+      github: "https://github.com/",
+      webapp: "https://github.com/",
+    },
+    {
+      id: 1,
+      title: "ThriveWell- A Holistic Health Portal",
+      description:
+        "ThriveWell is a holistic health portal developed with a React-based frontend, aimed at promoting global well-being in alignment with the United Nations Sustainable Development Goal 3 (Good Health and Well-Being). The platform offers users access to engaging features such as health statistics, wellness tips, quizzes, surveys, and curated resources. Designed with responsiveness in mind, ThriveWell ensures seamless access across devices. A key enhancement includes a streamlined workflow for managing wellness-related expenses, improving the overall user experience. Built with modular, reusable React components and deployed on Vercel, ThriveWell reflects a modern, efficient, and health-centric digital experience.",
+      image: thrivewellLogo,
+      tags: ["HTML", "CSS", "JavaScript", "React.js"],
+      github: "https://github.com/ajju-devs/ThriveWell",
+      webapp: "https://thrivewell-website.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "Reddit & YouTube CRUD App",
+      description:
+        "This is a full-featured content management application that allows users to create, update, and delete posts on Reddit and upload videos to YouTube through a single interactive interface. The app integrates seamlessly with the Reddit and YouTube APIs using praw and googleapiclient.discovery, providing support for text, image, and video posts on Reddit, and customizable video uploads on YouTube. The frontend is built with Streamlit to ensure a responsive and user-friendly experience, while the backend logic in Python handles authentication, error handling, and real-time API interactions. This project provided hands-on experience with third-party API integration, debugging, and live deployment of interactive applications.",
+      image: redditytcrudLogo,
+      tags: ["Python", "API", "HTML", "CSS", "Streamlit", "Reddit API (praw),", "YouTube Data API (googleapiclient.discovery)", "REST APIs", "OAuth 2.0", "Git", "VSCode"],
+      github: "https://github.com/ajju-devs/reddit-yt-crud-app",
+      webapp: "https://crud-project.streamlit.app/",
+    },
+  ];  
